443c494c86f702104a89574af3c288caa421fc8b
Deceptrax123 <srinitishsri@gmail.com>
2024-09-24 21:51:57 +0530

    isomorphism model

--- Initial_Training/Models/Model.py
+++ Initial_Training/Models/Model.py
@@ -1,51 +1,34 @@
 import torch
 from torch_geometric.nn import GCNConv, GraphNorm, global_mean_pool, EdgePooling, global_max_pool
-from torch.nn import Linear, BatchNorm1d, Module
+from torch.nn import Linear, BatchNorm1d, Module, ReLU
+from Initial_Training.Models.isomorphism import GraphIsomorphismNetwork
 import torch.nn.functional as F
 
 # Same model architecture for all problems
 
 
 class DrugClassificationModel(Module):
-    def __init__(self, input_features, num_features, num_labels, category):
+    def __init__(self, num_labels, encoder):
         super(DrugClassificationModel, self).__init__()
-        self.category = category
 
-        self.initial = GCNConv(in_channels=input_features,
-                               out_channels=1, normalize=True)
-        self.layer1 = GCNConv(in_channels=1,
-                              out_channels=num_features*2, normalize=True)
-        self.layer2 = GCNConv(in_channels=num_features*2,
-                              out_channels=num_features*4, normalize=True)
-        self.layer3 = GCNConv(in_channels=num_features*4,
-                              out_channels=num_features*8, normalize=True)
+        self.nn = GraphIsomorphismNetwork(encoder=encoder)
+        self.linear1 = Linear(in_features=512, out_features=256)
+        self.classifier = Linear(in_features=256, out_features=num_labels)
 
-        self.linear = Linear(in_features=num_features*8,
-                             out_features=num_features*16)
-        self.bn = BatchNorm1d(num_features*16)
-
-        self.classifier = Linear(
-            in_features=num_features*16, out_features=num_labels)
+        self.relu = ReLU()
+        self.bn = BatchNorm1d(num_features=256)
 
     def forward(self, v, edges, batch):
-        v = self.initial(v, edges)
-        v = F.relu(self.layer1(v, edges))
-        v = F.relu(self.layer2(v, edges))
-        v = F.relu(self.layer3(v, edges))
+        v = self.nn.forward(v, edges)
+        v = global_mean_pool(v, batch=batch)  # Graph View
 
-        v1 = global_mean_pool(v, batch)
-        v2 = global_max_pool(v, batch)
-        v = torch.add(v1, v2)
-
-        v = self.linear(v)
+        v = self.linear1(v)
         v = self.bn(v)
+        v = self.relu(v)
 
         v = self.classifier(v)
 
-        if self.category == 'binary' or self.category == 'multilabel':
-            return v, F.sigmoid(v)
-        else:
-            return v, F.softmax(v)
+        return v, F.sigmoid(v)
 
 
 class GraphClassificationModel(Module):
@@ -103,37 +86,3 @@ class GraphClassificationModel(Module):
             return v, F.sigmoid(v)
         else:
             return v, F.softmax(v, dim=1)
-
-
-class NodeClassificationModel(Module):
-    def __init__(self, num_features, num_labels):
-        super(NodeClassificationModel, self).__init__()
-
-        self.layer1 = GCNConv(in_channels=num_features,
-                              out_channels=num_features*2)
-        self.gn1 = GraphNorm(num_features*2)
-
-        self.layer2 = GCNConv(in_channels=num_features*2,
-                              out_channels=num_features*4)
-        self.gn2 = GraphNorm(num_features*4)
-
-        self.layer3 = GCNConv(in_channels=num_features*4,
-                              out_channels=num_features*8)
-        self.gn3 = GraphNorm(num_features*8)
-
-        self.classifier = GCNConv(
-            in_channels=num_features*8, out_channels=num_labels)
-
-    def forward(self, v, edges):
-        v = F.relu(self.layer1(v, edges))
-        v = self.gn1(v)
-
-        v = F.relu(self.layer2(v, edges))
-        v = self.gn2(v)
-
-        v = F.relu(self.layer3(v, edges))
-        v = self.gn3(v)
-
-        v = self.classifier(v)
-
-        return v, F.softmax(v)
---
+++ Initial_Training/Models/isomorphism.py
@@ -0,0 +1,58 @@
+
+import torch
+from torch.nn import Module, Linear, ReLU, BatchNorm1d, Dropout1d
+from torch_geometric.nn import GINConv, ChebConv
+import torch.nn.functional as F
+
+
+class GCNEncoder(Module):
+    def __init__(self):
+        super(GCNEncoder, self).__init__()
+        self.gcn1 = ChebConv(in_channels=86, out_channels=128, K=3)
+        self.gcn2 = ChebConv(in_channels=128, out_channels=256, K=3)
+        self.gcn3 = ChebConv(in_channels=256, out_channels=512, K=3)
+
+    def forward(self, v, edges):
+        x1 = F.relu(self.gcn1(v, edge_index=edges))
+        x2 = F.relu(self.gcn2(x1, edge_index=edges))
+        x3 = F.relu(self.gcn3(x2, edge_index=edges))
+
+        return x1, x2, x3
+
+
+class GraphIsomorphismNetwork(Module):
+    def __init__(self, encoder):
+        super(GraphIsomorphismNetwork, self).__init__()
+        self.gin1 = GINConv(nn=PhiLinearMapping(86, 128), train_eps=True)
+        self.gin2 = GINConv(nn=PhiLinearMapping(128, 256), train_eps=True)
+        self.gin3 = GINConv(nn=PhiLinearMapping(256, 512), train_eps=True)
+        self.gcn_enc = encoder
+
+    def forward(self, v, edges):
+        v1, v2, v3 = self.gcn_enc.forward(v, edges)
+        x1 = self.gin1(v, edge_index=edges)
+        xv1 = torch.add(x1, v1)
+
+        x2 = self.gin2(xv1, edge_index=edges)
+        xv2 = torch.add(x2, v2)
+
+        x3 = self.gin3(xv2, edge_index=edges)
+        xv3 = torch.add(x3, v3)
+
+        return xv3
+
+
+class PhiLinearMapping(Module):
+    def __init__(self, in_channels, out_channels):
+        super(PhiLinearMapping, self).__init__()
+        self.linear = Linear(in_features=in_channels,
+                             out_features=out_channels)
+        self.relu = ReLU()
+        self.bn = BatchNorm1d(num_features=out_channels)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        return x
