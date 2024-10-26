1158565f607e9e15960b6a9dec2a0d6aede1118e
Deceptrax123 <srinitishsri@gmail.com>
2024-10-26 17:16:13 +0530

    Create and test evaluation scripts

---
+++ Defense/HIV/eval.py
@@ -0,0 +1,203 @@
+from torch_geometric.loader import DataLoader
+from torch_geometric.utils import to_dense_adj, dense_to_sparse, erdos_renyi_graph
+from torch_geometric.utils import get_laplacian
+from torch_geometric.nn import GAE
+from Defense.HIV.hiv_dataset import HIVDataset
+import torch
+import numpy as np
+from Defense.Models.isomorphism import GCNEncoder, GCNEncoderNoise
+from Initial_Training.Models.Model import DrugClassificationModel as OriginalClassifier
+from Defense.Models.Model import DrugClassificationModel
+from Attack.contrastive_loss import InfoNCELoss
+from Attack.metrics import classification_binary_metrics
+from torch.utils.data import ConcatDataset
+from torch import nn
+from dotenv import load_dotenv
+import os
+import wandb
+import math
+import gc
+
+
+def unit_vector(z):
+    u1 = torch.randn(size=(1, z.size(1)))
+    u2 = torch.randn(size=(1, z.size(1)))
+    r1 = torch.sum(u1**2)**0.5
+    r2 = torch.sum(u2**2)**0.5
+
+    return u1/r1, u2/r2
+
+
+def attack(graphs):
+    _, _, z = autoencoder.encode(graphs.x, edges=graphs.edge_index)
+    zcap1, zcap2 = unit_vector(z)
+    epsilon1, epsilon2 = torch.normal(
+        0, torch.std(z)), torch.normal(0, torch.std(z))
+
+    z1 = torch.add(z, torch.mul(epsilon1, zcap1))
+    z2 = torch.add(z, torch.mul(epsilon2, zcap2))
+
+    adj_y = torch.sigmoid(torch.matmul(z, z.t()))
+    adj_y1 = torch.sigmoid(torch.matmul(z1, z1.t()))
+    adj_y2 = torch.sigmoid(torch.matmul(z2, z2.t()))
+
+    adj_y1 = torch.where(adj_y1 > 0.5, 1.0, 0.0)
+    adj_y2 = torch.where(adj_y2 > 0.5, 1.0, 0.0)
+
+    adj_x = to_dense_adj(edge_index=graphs.edge_index,
+                         max_num_nodes=graphs.num_nodes)
+
+    adj_x.requires_grad = True
+    adj_y1.requires_grad = True
+    adj_y2.requires_grad = True
+
+    loss = autoencoder.recon_loss(z, pos_edge_index=graphs.edge_index)+(LAMBDA*information_loss(
+        adj_y, adj_y1, adj_y2, adj_x))
+    loss.backward()
+
+    adj_x.retain_grad()
+    adj_y1.retain_grad()
+    adj_y2.retain_grad()
+
+    gradient_adj = adj_x.grad+adj_y1.grad+adj_y2.grad
+    mean_grad = torch.mean(gradient_adj)
+
+    adversarial_matrix = torch.where(gradient_adj > mean_grad, 1, 0)
+    adversarial_sparse, _ = dense_to_sparse(adversarial_matrix)
+
+    return adversarial_sparse, adversarial_matrix
+
+
+def attack_chance():
+    rand_number = torch.sin(torch.rand(1)*math.pi)
+
+    return rand_number
+
+
+def test():
+    epoch_loss = 0
+    epoch_acc = 0
+    epoch_prec = 0
+    epoch_rec = 0
+    epoch_auc = 0
+    epoch_f1 = 0
+    epoch_auc = 0
+
+    for step, graphs in enumerate(test_loader):
+        attack_prob = attack_chance()
+        # Generate Random Graph
+        er_graph = erdos_renyi_graph(
+            num_nodes=graphs.num_nodes, edge_prob=0.2)
+
+        graph_edges = graphs.edge_index
+        if attack_prob >= 0.5:
+            graph_edges, adv_matrix = attack(graphs)
+
+            # Laplacian of each graph
+            adversarial_laplacian, adv_weight = get_laplacian(graph_edges)
+            er_laplacian, er_weight = get_laplacian(er_graph)
+
+            # Eigen distribution
+            _, perturbed_eigen_vecs = torch.linalg.eig(to_dense_adj(
+                adversarial_laplacian, edge_attr=adv_weight, max_num_nodes=graphs.num_nodes))
+            _, er_eigen_vecs = torch.linalg.eig(to_dense_adj(
+                er_laplacian, edge_attr=er_weight, max_num_nodes=graphs.num_nodes))
+
+            # Similarity between eigen distributions
+            eigen_distribution_similarity = torch.mean(torch.cosine_similarity(
+                perturbed_eigen_vecs.real, er_eigen_vecs.real))
+
+            acti_vector = torch.tensor(
+                [eigen_distribution_similarity, 1-eigen_distribution_similarity, 1])
+        else:
+            norm_laplacian, norm_weight = get_laplacian(graph_edges)
+            er_laplacian, er_weight = get_laplacian(er_graph)
+
+            _, norm_eigen_vecs = torch.linalg.eig(
+                to_dense_adj(norm_laplacian, edge_attr=norm_weight, max_num_nodes=graphs.num_nodes))
+            _, er_eigen_vecs = torch.linalg.eig(to_dense_adj(
+                er_laplacian, edge_attr=er_weight, max_num_nodes=graphs.num_nodes))
+
+            eigen_distribution_similarity = torch.mean(torch.cosine_similarity(
+                norm_eigen_vecs.real, er_eigen_vecs.real))
+
+            acti_vector = torch.tensor(
+                [eigen_distribution_similarity, 1-eigen_distribution_similarity, 0])
+
+        logits, predictions = model_attack(
+            graphs.x, attack_vector=acti_vector, edges=graph_edges, batch=graphs.batch)
+        target_col = graphs.y.view(graphs.y.size(0), 1)
+
+        loss = loss_trainable(logits, target_col.float())
+
+        epoch_loss += loss.item()
+
+        acc, f1, prec, rec, auc = classification_binary_metrics(
+            predictions, target_col)
+        epoch_acc += acc.item()
+        epoch_prec += prec.item()
+        epoch_f1 += f1.item()
+        epoch_rec += rec.item()
+        epoch_auc += auc.item()
+
+        # print("Step Train Values")
+        # print("Epoch Loss: ", epoch_loss)
+        # print("Epoch Accuracy: ", epoch_acc)
+        # print("Epoch Precision: ", epoch_prec)
+        # print("Epoch F1: ", epoch_f1)
+        # print("Epoch AUC: ", epoch_auc)
+
+        del graphs, predictions, logits
+        gc.collect()
+
+    return epoch_loss/(step+1), epoch_acc/(step+1), epoch_prec/(step+1), epoch_rec/(step+1), epoch_f1/(step+1), \
+        epoch_auc/(step+1)
+
+
+if __name__ == '__main__':
+    torch.multiprocessing.set_sharing_strategy('file_system')
+    torch.autograd.set_detect_anomaly(True)
+    load_dotenv('.env')
+    test_set = HIVDataset(fold_key='Fold8', root=os.getenv(
+        "graph_files")+"/Fold8"+"/data/", start=35987)
+
+    params = {
+        'batch_size': 128,
+        'shuffle': True,
+        'num_workers': 0
+    }
+
+    test_loader = DataLoader(test_set, **params)
+
+    r_enc_normal = GCNEncoder()
+    model = OriginalClassifier(num_labels=1, encoder=r_enc_normal)
+    model.load_state_dict(torch.load(
+        os.getenv("graph_weights"), weights_only=True), strict=False)
+    model.eval()
+
+    embedder = model.nn.gcn_enc
+    autoencoder = GAE(encoder=embedder)
+
+    # Attack robust encoder
+    r_enc_attack = GCNEncoderNoise()
+    model_attack = DrugClassificationModel(num_labels=1, encoder=r_enc_attack)
+    model_attack.load_state_dict(torch.load(
+        f="Defense/HIV/weights/fine_tuned/model_10.pth", weights_only=True), strict=False)
+    model_attack.eval()
+
+    LAMBDA = 4e-1
+    LR = 2e-4
+    BETAS = (0.9, 0.999)
+    information_loss = InfoNCELoss()
+    loss_trainable = nn.BCEWithLogitsLoss()
+
+    optimizer = torch.optim.Adam(model_attack.parameters(), lr=LR, betas=BETAS)
+
+    test_loss, test_acc, test_prec, test_rec, test_f1, test_auc = test()
+    print("------------Test Metrics-------------")
+    print(f"Test Loss: {test_loss}")
+    print(f"Test Accuracy: {test_acc}")
+    print(f"Test Precision: {test_prec}")
+    print(f"Test Recall: {test_rec}")
+    print(f"Test F1: {test_f1}")
+    print(f"Test AUC: {test_auc}")
